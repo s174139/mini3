593c6d6e4d3a44cacbbcf88e29abb03dd7204adc
Gustav Larsen <redgustavlarsen@gmail.com>
2020-04-23 16:06:04 +0200

    Update visualize.py

--- visualize.py
+++ visualize.py
@@ -1,8 +1,9 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 import seaborn as sns; sns.set(style="white", color_codes=True)
 
-data = pd.read_csv("data_17.csv")
-
+data = pd.read_csv("data_32.csv")
+data = data.loc[:, data.columns != 'Unnamed: 0']
 means = data.mean(axis=0)
 variance = data.var(axis=0)
 correlation = data.corr(method='pearson')
@@ -14,9 +15,15 @@ print(correlation)
 ### --- plot ----
 #ax = data.plot.hist(axis= 0, bins=12, alpha=0.5)
 #density = data.plot.kde()
-s = data.iloc[:,2]
-t = data.iloc[:,3]
-sns.jointplot("T", "S", data=data)
+#sns.pairplot(data, height=2.5);
+
+data.hist(bins=20, range=[-10,10])
+plt.show()
+
+g = sns.PairGrid(data, palette='RdBu_r')
+g.map(plt.scatter, alpha=0.8)
+
+plt.show()
 
 #print(s)
 
